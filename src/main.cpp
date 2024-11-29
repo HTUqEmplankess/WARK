f82b5e36a13cc466c0b7b9d8f25d1441e383ee20
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-11-29 17:31:35 +0700

    [update] Wifi setup and [update] monitor speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <Wifi.h>
+#include <WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Network Credentials
+const char *ssid = "HTUq";
+const char *password = "22_07_66";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting To WiFi Network");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nConnected To The WiFi Network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
