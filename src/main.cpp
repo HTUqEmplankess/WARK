167427fcbe9cefaf5c5bf29d27efec9224b4e972
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-11-30 15:50:08 +0700

    [fix] algo and [update] .ino

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 #include <NewPing.h>
 
 // Pin definitions
-const int pirPin = 14;       // PIR Motion Sensor (Digital)
-const int dhtPin = 15;       // DHT22 Data Pin
-const int trigPin = 5;       // Ultrasonic Trig Pin 
-const int echoPin = 18;      // Ultrasonic Echo Pin 
-const int relayPin = 23;     // Relay Pin for controlling 12V LED
-const int i2cSDA = 21;          // I2C SDA
-const int i2cSCL = 22;          // I2C SCL
+const int pirPin = 14;   // PIR Motion Sensor (Digital)
+const int dhtPin = 15;   // DHT22 Data Pin
+const int trigPin = 5;   // Ultrasonic Trig Pin
+const int echoPin = 18;  // Ultrasonic Echo Pin
+const int relayPin = 23; // Relay Pin for controlling 12V LED
+const int i2cSDA = 21;   // I2C SDA
+const int i2cSCL = 22;   // I2C SCL
 
 // PIR
 int motionDetected = 0;
@@ -28,12 +28,13 @@ DHT dht(dhtPin, DHTTYPE);
 BH1750FVI lightMeter(BH1750_DEFAULT_I2CADDR, BH1750_ONE_TIME_HIGH_RES_MODE, BH1750_SENSITIVITY_DEFAULT, BH1750_ACCURACY_DEFAULT);
 
 // Ultrasonic setup
-NewPing sonar (trigPin, echoPin, 450);
+NewPing sonar(trigPin, echoPin, 450);
 
 // LCD setup
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Adjust address if needed
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Adjust address if needed
 
-void setup() {
+void setup()
+{
   // Serial monitor
   Serial.begin(115200);
 
@@ -44,41 +45,39 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
 
-
   // Relay setup
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);  
+  digitalWrite(relayPin, HIGH);
 
   // DHT22 setup
   dht.begin();
 
   // BH1750 Light Sensor setup (I2C)
-  Wire.begin(i2cSDA, i2cSCL);  // Initialize I2C on GPIO21 (i2cSDA) and GPIO22 (i2cSCL)
-  if (!lightMeter.begin()) {
+  Wire.begin(i2cSDA, i2cSCL); // Initialize I2C on GPIO21 (i2cSDA) and GPIO22 (i2cSCL)
+  if (!lightMeter.begin())
+  {
     Serial.println("BH1750 initialization failed!");
     lcd.print("BH1750 Error!");
   }
   /* change BH1750 settings on the fly */
-  lightMeter.setCalibration(1.06);                           //call before "readLightLevel()", 1.06=white LED & artifical sun
-  lightMeter.setSensitivity(1.00);                           //call before "readLightLevel()", 1.00=no optical filter in front of the sensor
-  lightMeter.setResolution(BH1750_CONTINUOUS_HIGH_RES_MODE); //call before "readLightLevel()", continuous measurement with 1.00 lux resolution
+  lightMeter.setCalibration(1.06);                           // call before "readLightLevel()", 1.06=white LED & artifical sun
+  lightMeter.setSensitivity(1.00);                           // call before "readLightLevel()", 1.00=no optical filter in front of the sensor
+  lightMeter.setResolution(BH1750_CONTINUOUS_HIGH_RES_MODE); // call before "readLightLevel()", continuous measurement with 1.00 lux resolution
 
   // LCD setup
-  lcd.begin(16, 2);  // Initialize LCD with 16 columns and 2 rows
+  lcd.begin(16, 2); // Initialize LCD with 16 columns and 2 rows
   lcd.backlight();
   lcd.print("Initializing...");
   delay(2000);
   lcd.clear();
-  digitalWrite(relayPin, LOW);  
-
-
+  digitalWrite(relayPin, LOW);
 }
 
-// ! Recommended to TEST 1 BY 1 device - uncomment each part of the loop 
+// ! Recommended to TEST 1 BY 1 device - uncomment each part of the loop
 
+void loop()
+{
 
-void loop() {
-  
   // // 2. PIR Motion Sensor : TEST Pass (too weak / relay is OK)
   // // lcd.clear();
   // delay(pirTime);
@@ -137,47 +136,50 @@ void loop() {
   //   lcd.setCursor(14, 0);
   //   lcd.print("lx");
   // }
-  
+
   // // 5. Ultrasonic Sensor: TEST Pass (2cm - 200cm - 450cm for far range the obj should be big too.)
   // // lcd.clear();
   // float distance = sonar.ping_cm() ; // Calculate distance in cm
-  
+
   // Serial.print("> ");
   // Serial.print("Distance: ");
   // Serial.print(distance);
   // Serial.println(" cm, ");
-  
+
   // lcd.setCursor(0, 0);
   // lcd.print("Dist:");
   // lcd.print(distance);
   // lcd.setCursor(12, 0);
-  // lcd.print(" cm");  
+  // lcd.print(" cm");
 
-    // 6. Ultrasonic and Pir Sensor: TEST Pass 
-    // lcd.clear();
+  // 6. Ultrasonic and Pir Sensor: TEST Pass
+  // lcd.clear();
   delay(pirTime);
-  double distance = sonar.ping_cm() ; // Calculate distance in cm
+  double distance = sonar.ping_cm(); // Calculate distance in cm
   motionDetected = digitalRead(pirPin);
-  
+
   Serial.print("> ");
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm, ");
-  
+
   lcd.setCursor(0, 0);
   lcd.print("Dist:");
   lcd.print(distance);
   lcd.setCursor(12, 0);
-  lcd.print(" cm");  
-  
+  lcd.print(" cm");
+
   Serial.print("> Motion: ");
   lcd.setCursor(0, 1);
   lcd.print("Motion:");
-  if (motionDetected == LOW ) {
+  if (motionDetected == LOW)
+  {
     Serial.println("0");
     lcd.setCursor(8, 1);
     lcd.print("None");
-  } else {
+  }
+  else
+  {
     Serial.println("1");
     lcd.setCursor(8, 1);
     lcd.print("Have");
@@ -186,15 +188,16 @@ void loop() {
   delay(500);
   lcd.clear();
 
-  if(motionDetected == HIGH && distance>0.0 ){
+  if (motionDetected == HIGH && ((distance > 0.0) && (distance < 200.0)))
+  {
     Serial.println("> LED : ON");
     lcd.print("LED ON");
     digitalWrite(relayPin, HIGH); // Turn ON relay (12V LED ON)
   }
-  else{
+  else
+  {
     Serial.println("> LED : OFF");
     lcd.print("LED OFF");
-    digitalWrite(relayPin, LOW);  // Turn OFF relay (12V LED OFF)
-
+    digitalWrite(relayPin, LOW); // Turn OFF relay (12V LED OFF)
   }
 }
