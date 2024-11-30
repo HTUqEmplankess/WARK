21490608e347e8378eb06f68495c38ed119f1f52
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-11-30 14:24:14 +0700

    [update] add UART2 and [update] comment for easy to watch code

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,28 @@
 #include <Wifi.h>
 #include <WebServer.h>
 
+#define RXD 16 // Receiver UART2
+#define TXD 17 // Transmitter UART2
+
 // Network Credentials
 const char *ssid = "HTUq";
 const char *password = "22_07_66";
 
 void setup()
 {
+  // Serial monitor
   Serial.begin(115200);
+
+  // UART2
+  Serial2.begin(9600, SERIAL_8N1, RXD, TXD);
+
+  // Wifi Station mode
   WiFi.mode(WIFI_STA);
+
+  // Wifi ID & Password setup
   WiFi.begin(ssid, password);
+
+  // Wifi Status Check
   Serial.print("Connecting To WiFi Network");
   while (WiFi.status() != WL_CONNECTED)
   {
